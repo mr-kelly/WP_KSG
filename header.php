ec8b6f00f287032a0272554275544ea3c66c2579
Mrkelly <chepy.v@gmail.com>
2011-07-05 16:13:34 +0800

    Add Page Title

--- header.php
+++ header.php
@@ -5,7 +5,29 @@
 <html lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>new_file</title>
+		<title>
+			<?php
+				// 页面标题
+				wp_title( '|', true, 'right');
+				
+				// 网站名称
+				bloginfo('name');
+				
+				// 如果是首页显示站点简介...
+				$site_description = get_bloginfo('description', 'display');
+				if ( is_home() || is_front_page() ) {
+					echo " - $site_description";	
+				}
+				
+				// 标题添加页码
+				global $page, $paged;
+				if ( $paged >= 2 || $page >= 2 ) {
+					echo ' - ' . sprintf(_( 'Page %s', 'seasungame' ), max( $paged, $page ));
+				}
+				
+				
+			?>
+		</title>
 		<meta name="generator" content="Studio 3 http://aptana.com/">
 		<meta name="author" content="Administrator">
 		<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
