8f41169cd38004cf352740d1426a2c834d594fab
ewzbw <ewzbw@163.com>
2011-07-05 16:58:13 +0800

    style.css

--- footer.php
+++ footer.php
@@ -1,5 +1,17 @@
 		<div id="footer">
-			Footer
+			<div id="site-info">
+				Goto
+				<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<?php bloginfo( 'name' ); ?>
+				</a>
+			</div><!-- #site-info -->
 		</div>
+		<?php
+			/* Always have wp_footer() just before the closing </body>
+	 		* tag of your theme, or you will break many plugins, which
+	 		* generally use this hook to reference JavaScript files.
+	 		*/
+			//wp_footer();
+		?>
 	</body>
 </html>
