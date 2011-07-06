492be9a7b8f81ab8490a37f7f24da1b69f62cfe0
ewzbw <ewzbw@163.com>
2011-07-06 16:20:56 +0800

    js

--- header.php
+++ header.php
@@ -49,8 +49,18 @@
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.backgroundPosition.js"></script>
 		<script type="text/javascript">
 			
+			<?php  // 获取图片相关信息
+				$home_window_bg_path=get_template_directory_uri();
+				$home_window_bg_path=$home_window_bg_path.'/images/bg_test.JPG';
+				$arr=getimagesize($home_window_bg_path);
+				//echo $arr[3];
+				$strarr=explode("\"",$arr[3]);
+				//$strarr[1]是宽     $strarr[3]是高;
+				echo 'var home_window_width='.$strarr[1];
+			?>
+			
 			// 变量申明
-			var home_window_pos=-200;
+			var home_window_pos=-960;
 			
 			// jQuery document.ready
 			jQuery(document).ready(function(){
@@ -58,7 +68,7 @@
 				// #home_window 图片自动播放
 				jQuery('#home_window').css("display", "none");
 				jQuery('#home_window').fadeIn(1000);
-				setInterval('home_window_move()', 1000);
+				setInterval('home_window_move()', 2000);
 				
 				//jQuery('.home_news_box').css("display", "none");
 				//jQuery('.home_news_box').fadeIn(1000);
@@ -68,7 +78,10 @@
 			// functions
 			function home_window_move(){   // 控制图片   #home_window 处
 				jQuery('#home_window').stop().animate({backgroundPosition: '('+ home_window_pos + 'px 0px)'}, 1000);
-				home_window_pos-=200;
+				home_window_pos-=960;
+				if (home_window_pos<-home_window_width){
+					home_window_pos=0;
+				}
 			}
 			
 		</script>
